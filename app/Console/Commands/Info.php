ab75771604f548bf0fe8b53295813080cca923ed
Artyom <Karaxanyan.1997@mail.ru>
2022-08-31 17:35:34 +0400

    use crone job

---
+++ app/Console/Commands/Info.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class Info extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'insert:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $randomPass=   rand(2500,5555);
+        $randomString = Str::random(10);
+
+        $users= new User();
+        $name = 'Name';
+        $password = Hash::make($randomPass);
+        $email = $randomString.'@gmail.com';
+
+        $users->name=$name;
+        $users->email=$email;
+        $users->password=$password;
+        $users->save();
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use  App\Console\Commands\Info;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,9 +13,13 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        Info::class
+    ];
+
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+//        $schedule->command('insert:users')->weekly();
     }
 
     /**
